945ccdc096998863669d19650680e8c9ebbb2840
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-12-06 15:19:53 +0000

    Create Coin.cs
    Script for removing coins as they are collected

---
+++ Honours/Assets/Scripts/Coin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{   
+    public GameObject coin; 
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Player"){
+            coin.SetActive(false);
+        }
+    }
+}
