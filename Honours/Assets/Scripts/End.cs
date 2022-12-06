ad8886a6baa0e4915710fb49d80059c667dfc02b
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-12-06 14:51:09 +0000

    Prefab overhaul
    - Added tags to prefabs

--- Honours/Assets/Scripts/End.cs
+++ Honours/Assets/Scripts/End.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class End : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other) {
+    private void OnCollisionEnter2D(Collision2D other) {
         
     }
 }
